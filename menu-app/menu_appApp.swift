3fa0ce74158e2bedaaa9f8f2716bd1c0bd95c078
alexapvl <alex.robite@gmail.com>
2024-07-01 01:45:21 +0300

    initial commit

---
+++ menu-app/menu_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  menu_appApp.swift
+//  menu-app
+//
+//  Created by Alexandru Apavaloaiei on 01.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct menu_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
